b7632563e9767d09cb25e15b6c29b974e1ea9f89
saatvikix <saatvik.cloud@gmail.com>
2026-07-29 19:51:29 +0530

    Dynamic table creation, wrapped as function

--- assignment-05/web/templateLiterals.js
+++ assignment-05/web/templateLiterals.js
@@ -1,20 +1,24 @@
 const users = [
-    {name: "Alice", age: 53},
-    {name: "Tim", age: 74}
+    { name: "Alice", age: 53 },
+    { name: "Tim", age: 74 }
 ];
 
-const body = document.getElementsByTagName("body")[0];
-const table = document.createElement("table");
+function generateTable() {
+    const body = document.getElementsByTagName("body")[0];
+    const table = document.createElement("table");
 
-const rows = users.map(user => {
-    return `
+    const rows = users.map(user => {
+        return `
         <tr>
             <td>${user.name}</td>
             <td>${user.age}</td>
         </tr>
     `;
-});
+    });
 
-rows.forEach(row => table.innerHTML += row);
+    rows.forEach(row => table.innerHTML += row);
 
-body.appendChild(table);
+    body.appendChild(table);
+}
+
+generateTable();
