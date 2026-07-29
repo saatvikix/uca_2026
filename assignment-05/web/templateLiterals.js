2ae113edfe4abfeb1ce95ec40d8508cad5422377
saatvikix <saatvik.cloud@gmail.com>
2026-07-29 19:49:06 +0530

    Dynamic table using map() and literals

---
+++ assignment-05/web/templateLiterals.js
@@ -0,0 +1,20 @@
+const users = [
+    {name: "Alice", age: 53},
+    {name: "Tim", age: 74}
+];
+
+const body = document.getElementsByTagName("body")[0];
+const table = document.createElement("table");
+
+const rows = users.map(user => {
+    return `
+        <tr>
+            <td>${user.name}</td>
+            <td>${user.age}</td>
+        </tr>
+    `;
+});
+
+rows.forEach(row => table.innerHTML += row);
+
+body.appendChild(table);
